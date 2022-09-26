79df96a18b1cbc438c50edf0dd5f190c18fdc512
ethicweb <114440368+ethicweb@users.noreply.github.com>
2022-09-26 20:05:54 +0000

    Deploying to gh-pages from @ ethicweb/website@d945554daa2cf3a7283368dc457fe8440e62df44 🚀

---
+++ _next/static/chunks/pages/home-dcfef6f4544b8682.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[229],{4309:function(a,b,c){(window.__NEXT_P=window.__NEXT_P||[]).push(["/home",function(){return c(7084)}])},7084:function(a,b,c){"use strict";c.r(b);var d=c(5893),e=function(){return(0,d.jsx)(d.Fragment,{children:"HOME"})};b.default=e}},function(a){a.O(0,[774,888,179],function(){var b;return a(a.s=4309)}),_N_E=a.O()}])
---
+++ _next/static/dpWCgcOrK2B5lplpZbU4_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/30998cfcce19d8a1.css","static/chunks/pages/index-60fe0c120aee0698.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],"/home":["static/chunks/pages/home-dcfef6f4544b8682.js"],sortedPages:["/","/_app","/_error","/home"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
